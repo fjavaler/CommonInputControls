1e8c12d22feffcfe7c841047177ccd739b574773
Frederick Javalera <frederickjavalera@gmail.com>
2019-11-18 18:27:11 -0700

    commit

--- CommonInputControls/ViewController.swift
+++ CommonInputControls/ViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController
+{
+    @IBOutlet var button: UIButton!
+    @IBOutlet var toggle: UISwitch!
+    @IBOutlet var slider: UISlider!
+    @IBAction func respondToLongPress(_ sender: UILongPressGestureRecognizer)
+    {
+        let location = sender.location(in: view)
+        print(location)
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton)
+    {
+        print("Button was tapped!")
 
+        if toggle.isOn
+        {
+            print("The switch is on!")
+        } else {
+            print("The switch is off.")
+        }
 
+        print("The slider is set to \(slider.value)")
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch)
+    {
+        if (sender.isOn)
+        {
+            print("The switch is on!")
+        }
+        else
+        {
+            print("The switch is off.")
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider)
+    {
+        print(sender.value)
+    }
+    
+    
+    @IBAction func enterPressed(_ sender: UITextField)
+    {
+        if let text = sender.text
+        {
+            print(text)
+        }
+    }
+    
+    
+    @IBAction func textChanged(_ sender: UITextField)
+    {
+        if let text = sender.text
+        {
+            print(text)
+        }
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
 }
 
